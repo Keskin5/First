c0533a178f51f6be561046ce7c12969432d8fcdc
Keskin5 <130964010+Keskin5@users.noreply.github.com>
2023-04-21 23:34:29 +0100

    Add files via upload
     Lab: Working with files in Jupyter Notebooks

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+# # Objectives
+# 
+# This notebook covers some of the key takeaways from the course, including:
+# 
+# - Listing popular programming languages for Data Science
+# - Explaining basic concepts of statistical analysis and probability theory
+# - Demonstrating how to work with data using Pandas library
+# - Visualizing data using Matplotlib and Seaborn libraries
+# - Introducing machine learning algorithms and Scikit-learn library
+# 
+# Some of the popular languages that Data Scientists use are:
+# 1.Python
+# 2.R
+# 3.SQL
+# 
+# Some of the commonly used libraries used by Data Scientists include:
+# 1.NumPy
+# 2.Pandas
+# 3.Matplotlib
+# 
+# | Data Science Tools |
+# | --- |
+# | Jupyter Notebook |
+# | RStudio |
+# | Spyder |
+# 
+# 
+# 
+
+# In[1]:
+
+
+
+# Below are a few examples of evaluating arithmetic expressions in Python
+
+##This a simple arithmetic expression to mutiply then add integers.
+
+(3*4)+5
+
+
+# In[4]:
+
+
+##This will convert 200 minutes to hours by diving by 60.
+200/60
+
+
+# ## Author
+# 
+# Keskin Karaagac
+
+# In[ ]:
+
+
+
+
